e1da52d9bff5450ab685a23e5cd10a804a0dde6c
Adel M. Abdallah <amabdallah@aggiemail.usu.edu>
2015-07-06 12:24:47 -0600

    update

---
+++ Files/WorkshopQuries/02DiscoverAvailableDataToExpandBear.sql
@@ -0,0 +1,95 @@
+﻿/*
+This query discovers the available data to expand the existing Lower Bear River Model in the Water and Evaluation 
+and Assessment (WEAP) model to the entire Bear Watershed 
+
+The search is based on a boundary of coordinates for instances in the upper Bear River Watershed. 
+Users need to only change these coordinates to discover data in other regions 
+
+
+Result:
+Users can see the number of available instances and attributes for object types that WEAP requires. 
+They can see the sources of each Object types and the native name in its original source along with the 
+controlled or common name. 
+
+Users can further search for more metadata and data about these instances. 
+Then they can choose which ones to import to their model 
+
+*/
+
+-- Select the available Object Types that match with what WEAP is looking for.
+-- Controlled vocabularies of Object Types is the mediator between WEAP and the other sources semantics 
+
+SELECT  DISTINCT "ObjectTypes"."NativeObjectName","CommonObjectType",
+-- Count the number of instances that are available for each Object Type
+COUNT(DISTINCT "Instances"."InstanceName") AS "#Of Instances",
+-- Count the number of attributes that are available for each Object type 
+COUNT (DISTINCT "Attributes"."NativeAttributeName") AS "#Ofattributes",
+"MasterNetworkName" 
+
+FROM "DataStructures"
+
+-- Left join the Object types with the select Data Structure 
+Left JOIN "ObjectTypes" 
+ON "ObjectTypes"."DataStructureID"="DataStructures"."DataStructureID"
+
+Left JOIN "CommonObjectTypes" 
+ON "CommonObjectTypes"."CommonObjectTypeID"="ObjectTypes"."CommonObjectTypeID" 
+
+Left JOIN  "ObjectAttributes"
+ON "ObjectAttributes"."ObjectTypeID"="ObjectTypes"."ObjectTypeID"
+
+Left JOIN  "Attributes"
+ON "Attributes"."AttributeID"="ObjectAttributes"."AttributeID"
+
+Left JOIN "MetadataMapping"
+ON "MetadataMapping"."ObjectAttributeID"="ObjectAttributes"."ObjectAttributeID"
+
+Left JOIN "ScenarioMetadata"
+ON "ScenarioMetadata"."MetadataMappingID"="MetadataMapping"."MetadataMappingID"
+
+Left JOIN "Scenarios"
+ON "Scenarios"."ScenarioID"="ScenarioMetadata"."ScenarioID"
+
+Left JOIN "MasterNetworks" 
+ON "MasterNetworks"."MasterNetworkID"="Scenarios"."MasterNetworkID"
+
+LEFT JOIN "Instances"
+ON "Instances"."InstanceID"="MetadataMapping"."InstanceID"
+
+Left JOIN "Connections" 
+ON "Connections"."LinkInstanceID"="Instances"."InstanceID"
+
+Left JOIN "Instances" As "StartNodeInstance"
+ON "StartNodeInstance"."InstanceID"="Connections"."StartNodeInstanceID"
+
+
+WHERE
+-- specify the boundary of coordinates of the search domain in space 
+-- this Boundary is for the parts of the upper Bear River Watershed 
+("Instances"."Longitude">='-112.4424' 
+AND "Instances"."Longitude"<='-110.65833'
+AND "Instances"."Latitude">='40.83833'
+AND "Instances"."Latitude"<='42.84333') AND 
+
+
+-- Select the list of Object Types that only WEAP requires 
+("CommonObjectType" IN (SELECT  DISTINCT  "CommonObjectType" 
+
+FROM "DataStructures"
+
+Left JOIN "ObjectTypes" 
+ON "ObjectTypes"."DataStructureID"="DataStructures"."DataStructureID"
+
+Left JOIN "CommonObjectTypes" 
+ON "CommonObjectTypes"."CommonObjectTypeID"="ObjectTypes"."CommonObjectTypeID" 
+
+-- Query only the WEAP Data Structure   
+WHERE "DataStructureName"='WEAP') OR 
+
+-- Include the WaterBody and Streams Object Types 
+"CommonObjectType"='Water Body' OR "CommonObjectType"='Stream')
+
+
+GROUP BY
+"MasterNetworkName" ,"ObjectTypes"."NativeObjectName",
+"CommonObjectType"
---
+++ Files/WorkshopQuries/04DiscoverSearchAlabama.sql
@@ -0,0 +1,86 @@
+﻿ /*
+This query discovers the available data to to build a model in the Water and Evaluation 
+and Assessment (WEAP) system for the Tuscaloosa region, Alabama
+
+The search is based on a boundary of coordinates for instances in the Tuscaloosa region, Alabama
+Users need to only change these coordinates to discover data in other regions 
+
+
+Result:
+Users can see the Objects, their topology, and their Attributes that WEAP uses 
+to model water systems. These Object and Attributes are used as input to queries 
+to search and discover water management data in other data structures
+
+Users can further search for more metadata and data about these instances. 
+Then they can choose which ones to import to their model 
+
+*/
+
+-- Select the available Object Types that match with what WEAP is looking for.
+-- Controlled vocabularies of Object Types is the mediator between WEAP and the other sources semantics 
+
+SELECT  DISTINCT "Instances"."InstanceName","ObjectTypes"."NativeObjectName","CommonObjectType","ObjectTopology","MasterNetworkName"
+
+-- 
+FROM "DataStructures"
+
+Left JOIN "ObjectTypes" 
+ON "ObjectTypes"."DataStructureID"="DataStructures"."DataStructureID"
+
+Left JOIN "CommonObjectTypes" 
+ON "CommonObjectTypes"."CommonObjectTypeID"="ObjectTypes"."CommonObjectTypeID"
+
+Left JOIN  "ObjectAttributes"
+ON "ObjectAttributes"."ObjectTypeID"="ObjectTypes"."ObjectTypeID"
+
+Left JOIN  "Attributes"
+ON "Attributes"."AttributeID"="ObjectAttributes"."AttributeID"
+
+Left JOIN  "CommonAttributes"
+ON "CommonAttributes"."CommonAttributeID"="Attributes"."CommonAttributeID"
+
+Left JOIN "MetadataMapping"
+ON "MetadataMapping"."ObjectAttributeID"="ObjectAttributes"."ObjectAttributeID"
+
+Left JOIN "DataStorage" 
+ON "DataStorage"."DataStorageID"="MetadataMapping"."DataStorageID"
+
+Left JOIN "ScenarioMetadata"
+ON "ScenarioMetadata"."MetadataMappingID"="MetadataMapping"."MetadataMappingID"
+
+Left JOIN "Scenarios"
+ON "Scenarios"."ScenarioID"="ScenarioMetadata"."ScenarioID"
+
+Left JOIN "MasterNetworks" 
+ON "MasterNetworks"."MasterNetworkID"= "Scenarios"."MasterNetworkID"
+
+Left JOIN "Instances" 
+ON "Instances"."InstanceID"="MetadataMapping"."InstanceID"
+
+
+-- specify the boundary of coordinates of the search domain in space 
+-- this Boundary is for the Tuscaloosa region, Alabama
+WHERE  "Instances"."Longitude">='-88.00' AND "Instances"."Latitude">='33.00'
+AND "Attributes"."NativeAttributeName"='ObjectInstances' 
+
+AND "Instances"."Longitude"<='-87.00' AND "Instances"."Latitude"<='34.00'
+AND  "Attributes"."NativeAttributeName"='ObjectInstances'  AND
+
+("CommonObjectType" IN (SELECT  DISTINCT  "CommonObjectType" 
+
+FROM "DataStructures"
+
+Left JOIN "ObjectTypes" 
+ON "ObjectTypes"."DataStructureID"="DataStructures"."DataStructureID"
+
+Left JOIN "CommonObjectTypes" 
+ON "CommonObjectTypes"."CommonObjectTypeID"="ObjectTypes"."CommonObjectTypeID" 
+
+-- Query only the WEAP Data Structure   
+WHERE "DataStructureName"='WEAP') OR 
+
+-- Include the WaterBody and Streams Object Types 
+"CommonObjectType"='Stream')
+
+
+ORDER BY "InstanceName"
